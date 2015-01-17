e5b6ebea04c788f1c9a0211d813b8f629184680c
kimihiro_n <kimihiro_n@pistatium.dip.jp>
2015-01-17 15:14:54 +0900

    create project

---
+++ app/src/main/java/articon/lib/pistatium/appspot/com/articon/articon/Articon.java
@@ -0,0 +1,20 @@
+package articon.lib.pistatium.appspot.com.articon.articon;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+/**
+ * Created by kimihiro on 15/01/17.
+ */
+public class Articon {
+
+    private Context context;
+
+    public Articon(Context context) {
+        this.context = context;
+    }
+
+    public Bitmap generate(String string) {
+        return nil;
+    }
+}
---
+++ app/src/main/java/articon/lib/pistatium/appspot/com/articon/articon/ArticonConfig.java
@@ -0,0 +1,7 @@
+package articon.lib.pistatium.appspot.com.articon.articon;
+
+/**
+ * Created by kimihiro on 15/01/17.
+ */
+public class ArticonConfig {
+}
